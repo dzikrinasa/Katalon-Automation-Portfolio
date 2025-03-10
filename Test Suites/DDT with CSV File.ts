577598c547268e74f4c11cc846422b66bfb4d67a
Dzikri Nandi Asa <BSI90938@bsi.co.id>
2025-03-10 15:41:05 +0700

    Add Test Suites & Data Files for DDT With CSV & Excel Files

---
+++ Test Suites/DDT with CSV File.ts
@@ -0,0 +1,42 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DDT with CSV File</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>cad1fa99-f8cf-48d5-a152-919b15dff5b4</testSuiteGuid>
+   <testCaseLink>
+      <guid>b8dcfab2-7265-468f-af4c-9e7fd406a6ca</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Katalon Academy/Data-Driven Testing/Fine a place</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>1274a2c1-f930-4ec3-9448-0a504269de5e</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/CSV_Cities</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>1274a2c1-f930-4ec3-9448-0a504269de5e</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>city</value>
+         <variableId>c9adc9f4-926e-4193-a89b-3af07b710c06</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>1274a2c1-f930-4ec3-9448-0a504269de5e</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>expectedResult</value>
+         <variableId>3212d94f-8819-428e-bc02-13b9ef366d94</variableId>
+      </variableLink>
+   </testCaseLink>
+</TestSuiteEntity>
